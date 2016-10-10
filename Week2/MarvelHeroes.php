6f160af631e503e6132712a10de502ba1ab54ca2
1611267 <phaseiimusic@hotmail.com>
2016-10-10 16:38:37 +0100

    Initial commit

--- Week2/MarvelHeroes.php
+++ Week2/MarvelHeroes.php
@@ -6,23 +6,7 @@
  * Time: 15:45
  */
 
-define('hostname','us-cdbr-azure-southcentral-f.cloudapp.net');
-define('username','b506d8420b1df6');
-define('password','e2ca163b');
-define('db_name','jm1611267db');
-
-// Connect to csdm webdev server and select database
-$db = new mysqli(
-    hostname,
-    username,
-    password,
-    db_name
-);
-
-// Test if connection was established, and print any errors
-if($db->connect_errno) {
-    die('Connect failed['.$db->connect_error.']');
-}
+include 'dbConnect.php';
 
 // Create a SQL Query as a String
 $sql_query = "SELECT * FROM marvelmovies";
