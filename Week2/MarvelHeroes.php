c8e0b05c8880ffefc76448b214a3b492d6b4e5fe
1611267 <phaseiimusic@hotmail.com>
2016-10-10 16:30:57 +0100

    Initial commit

--- Week2/MarvelHeroes.php
+++ Week2/MarvelHeroes.php
@@ -28,12 +28,49 @@ if($db->connect_errno) {
 // Create a SQL Query as a String
 $sql_query = "SELECT * FROM marvelmovies";
 // Execute the SQL Query
-$reult = $db->query($sql_query);
+$result = $db->query($sql_query);
 
 // Iterate over $result object, one $row at a time
 // Use fetch_array() to return an asociative array
 while($row = $result->fetch_array()) {
     echo "<p>".$row['superheroName']."</p>";
+}
+
+
+
+// Create a SQL Query as a String
+$sql_query = "SELECT * FROM marvelmovies WHERE productionStudio LIKE 'Marvel Studios'";
+// Execute the SQL Query
+$result = $db->query($sql_query);
+
+// Iterate over $result object, one $row at a time
+// Use fetch_array() to return an asociative array
+while($row = $result->fetch_array()) {
+    echo "<p>".$row['title']."</p>";
+}
+
+
+// Create a SQL Query as a String
+$sql_query = "SELECT * FROM marvelmovies WHERE yearReleased > 2010";
+// Execute the SQL Query
+$result = $db->query($sql_query);
+
+// Iterate over $result object, one $row at a time
+// Use fetch_array() to return an asociative array
+while($row = $result->fetch_array()) {
+    echo "<p>".$row['title']."</p>";
+}
+
+
+// Create a SQL Query as a String
+$sql_query = "SELECT * FROM marvelmovies WHERE title LIKE '%X-Men%'";
+// Execute the SQL Query
+$result = $db->query($sql_query);
+
+// Iterate over $result object, one $row at a time
+// Use fetch_array() to return an asociative array
+while($row = $result->fetch_array()) {
+    echo "<p>".$row['title']."</p>";
 }
 
 $result->close();
