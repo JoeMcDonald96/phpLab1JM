79894e31e5d4bb29109394eee3841c49e8ecc82d
1611267 <phaseiimusic@hotmail.com>
2016-10-10 16:26:22 +0100

    Initial commit

--- Week2/MarvelHeroes.php
+++ Week2/MarvelHeroes.php
@@ -6,17 +6,14 @@
  * Time: 15:45
  */
 
-define('hostname','us-cdbr-azure-southcentral-f.cloudapp.net');
-define('username','b506d8420b1df6');
-define('password','e2ca163b');
-define('db_name','jm1611267db');
+
 
 // Connect to csdm webdev server and select database
 $db = new mysqli(
-    hostname,
-    username,
-    password,
-    db_name
+    'us-cdbr-azure-southcentral-f.cloudapp.net',
+    'b506d8420b1df6',
+    'e2ca163b',
+    'jm1611267db'
 );
 
 // Test if connection was established, and print any errors
