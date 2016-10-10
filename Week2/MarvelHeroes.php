037fefba6ed41de96ec1b0f00a257b85c0ea1c33
1611267 <phaseiimusic@hotmail.com>
2016-10-10 16:17:23 +0100

    Initial commit

--- Week2/MarvelHeroes.php
+++ Week2/MarvelHeroes.php
@@ -6,6 +6,13 @@
  * Time: 15:45
  */
 
+include("dbConnect.php");
+
+// Test if connection was established, and print any errors
+if($db->connect_errno) {
+    die('Connect failed['.$db->connect_error.']');
+}
+
 // Create a SQL Query as a String
 $sql_query = "SELECT * FROM marvelmovies";
 // Execute the SQL Query
