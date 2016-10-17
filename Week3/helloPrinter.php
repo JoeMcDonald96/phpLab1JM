3b14cc03bb185e67aa16fdb864e472d3cf005f76
1611267 <phaseiimusic@hotmail.com>
2016-10-17 14:09:30 +0100

    Initial commit

--- Week3/helloPrinter.php
+++ Week3/helloPrinter.php
@@ -4,10 +4,10 @@
 
     </head>
     <body>
+    <?php
+        $planet = $_GET[$planet];
+        echo "<p> Hello {$planet}</p>"
+    ?>
 
     </body>
 </html>
-<?php
-    $planet = $_GET[$planet];
-    echo "<p> Hello {$planet}</p>"
-?>
