00df323cdfe5b6c968ba637f407bb3fa4701a5ce
1611267 <phaseiimusic@hotmail.com>
2016-10-17 14:07:40 +0100

    Initial commit

--- Week1/index.php
+++ Week1/index.php
@@ -3,10 +3,14 @@
 <head>
 </head>
 <body>
+
+<p> 'Where would you like to say hello?' </p>
+<p> <a href='http://phplab1jm.azurewebsites.net/Week3/helloPrinter.php?planet=Earth'>Earth</a></p>
+<p> <a href='http://phplab1jm.azurewebsites.net/Week3/helloPrinter.php?planet=Mars'>Mars</a></p>
+<p> <a href='http://phplab1jm.azurewebsites.net/Week3/helloPrinter.php?planet=Uranus'>Uranus</a></p>
 <p>
     <?php
     echo "Hello World";
-    include'helloSelector.php';
     ?>
 </p>
 </body>
