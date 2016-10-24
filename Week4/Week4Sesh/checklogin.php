acf680c7249a8429ab985e1a97a2c2c2d59735ab
1611267 <phaseiimusic@hotmail.com>
2016-10-24 14:56:54 +0100

    Initial commit

--- Week4/Week4Sesh/checklogin.php
+++ Week4/Week4Sesh/checklogin.php
@@ -3,6 +3,9 @@
 $username = $_POST["name"];
 $password = $_POST["password"];
 
+$_SESSION['username'] = $username;
+$_SESSION['password'] = $password;
+
 if($username == "username" && $password == "password") {
     session_start();
     $_SESSION['access_level_session'] = "standarduser";
